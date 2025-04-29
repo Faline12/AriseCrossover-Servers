a71b891a95367198e460fae21abe56f96ef45134
Faline12 <62182664+Faline12@users.noreply.github.com>
2025-04-29 09:04:04 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 // tailwind.config.js
 
 module.exports = {
-  darkMode: 'class', // Enables class-based dark mode
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        discordDark: '#2f3136', // Dark theme background color (Discord-like)
-        discordLight: '#ffffff', // Light theme background color
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
